e3140809d7617d3e143d7b4017b5cdc622395f1c
Abdul-Malik <82204978+abdul-malik360@users.noreply.github.com>
2021-11-06 20:38:12 +0200

    all data added on projects, now to complete styling

--- js/projects.js
+++ js/projects.js
@@ -2,133 +2,157 @@ let projects = [
   {
     imgURL: "../images/projects/timeline.PNG",
     imgALT: "My Timeline",
-    title: "My Project Title 45",
+    title: "Timeline",
     techStack: "HTML/CSS",
-    description: "lorem ipsum JS",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "timeline-modal",
+    description: "A timeline of work experience using AOS(Animate On Scroll).",
+    githubURL: "https://github.com/abdul-malik360/My_Timeline",
+    liveProjectURL: "https://youthful-ride-5ab649.netlify.app/",
+    id: "timeline",
   },
   {
     imgURL: "../images/projects/contactform.PNG",
-    imgALT: "My Project 1",
-    title: "My Project Title 1",
+    imgALT: "A contact form",
+    title: "Contact Form",
     techStack: "HTML/CSS",
-    description: "lorem ipsum hello world",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "contact-modal",
+    description: "A contact form using Glassmorphism.com and Formspree.",
+    githubURL: "https://github.com/abdul-malik360/Contact_Form",
+    liveProjectURL: "https://pensive-jepsen-852132.netlify.app/",
+    id: "contact-form",
   },
   {
     imgURL: "../images/projects/calculator.png",
-    imgALT: "My Project 2",
-    title: "My Project Title 2",
+    imgALT: "A javascript calculator",
+    title: "Calculator",
     techStack: "JavaScript",
-    description: "lorem ipsum Python",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "calculator-modal",
+    description: "A calculator created using Javascript",
+    githubURL: "https://github.com/abdul-malik360/jscalculator",
+    liveProjectURL: "https://gallant-jackson-9fbc52.netlify.app/",
+    id: "js-calculator",
   },
   {
     imgURL: "../images/projects/bmicalculator.PNG",
-    imgALT: "This is cool",
-    title: "Some Title",
+    imgALT: "A BMI Calculator",
+    title: "BMI Calculator",
     techStack: "Python",
-    description: "I made this",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "bmi-modal",
+    description: "A BMI Calculator",
+    githubURL:
+      "https://github.com/abdul-malik360/BMI_Calculator/blob/main/BMI_Calculator.py",
+    liveProjectURL:
+      "https://replit.com/@abdulmalik360/BMICalculator#BMI_Calculator.py",
+    id: "bmi-calculator",
   },
   {
     imgURL: "../images/projects/sneakersite.PNG",
-    imgALT: "My Project 4",
-    title: "My Project Title 4",
+    imgALT: "A sneaker site",
+    title: "Sneaker Site",
     techStack: "HTML/CSS",
-    description: "lorem ipsum",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "sneaker-modal",
+    description: "A sneaker site implementing the basics of a website",
+    githubURL: "https://github.com/abdul-malik360/Sneaker_Site",
+    liveProjectURL: "https://affectionate-bose-3983db.netlify.app/",
+    id: "sneaker-site",
   },
   {
     imgURL: "../images/projects/weatherapp.PNG",
-    imgALT: "My Project 5",
-    title: "My Project Title 5",
+    imgALT: "A Weather app",
+    title: "A Weather app",
     techStack: "Python",
-    description: "This is dynamic",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "weather-modal",
+    description: "A Weather app",
+    githubURL:
+      "https://github.com/abdul-malik360/requests/blob/main/challenge.py",
+    liveProjectURL: "https://replit.com/@abdulmalik360/requests#challenge.py",
+    id: "weather-app",
   },
   {
     imgURL: "../images/projects/testimonials.PNG",
-    imgALT: "My Project 6",
-    title: "My Project Title 6",
+    imgALT: "A testimonial slider",
+    title: "Testimonials",
     techStack: "HTML/CSS",
-    description: "I just created this",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "testimonial-modal",
+    description:
+      "A testimonial slider using a jQuery plugin called OWL Carousel.",
+    githubURL: "https://github.com/abdul-malik360/my_testomonials",
+    liveProjectURL: "https://confident-poincare-1690a0.netlify.app/",
+    id: "testimonial-slider",
   },
   {
     imgURL: "../images/projects/adding.PNG",
-    imgALT: "My Project 7",
-    title: "My Project Title 7",
+    imgALT: "An Addition Calculator",
+    title: "Addition Calculator",
     techStack: "Python",
-    description: "Pokedex",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "adding-modal",
+    description:
+      "Defining functions in python to create a program adding numbers",
+    githubURL:
+      "https://github.com/abdul-malik360/tkinter/blob/main/GUI_Task.py",
+    liveProjectURL: "https://replit.com/@abdulmalik360/tkinter#GUI_Task.py",
+    id: "adding-app",
   },
   {
     imgURL: "../images/projects/projectscard.PNG",
-    imgALT: "My Project 7",
-    title: "My Project Title 7",
+    imgALT: "A projects card",
+    title: "Projects Card",
     techStack: "HTML/CSS",
-    description: "Pokedex",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "project-modal",
+    description:
+      "A Project Card with links to all my completed projects using Glassmorphism.com.",
+    githubURL: "https://github.com/abdul-malik360/project_card",
+    liveProjectURL: "https://hardcore-minsky-0c2f2a.netlify.app/",
+    id: "projects-card",
   },
   {
     imgURL: "../images/projects/temperatureconverter.PNG",
-    imgALT: "My Project 7",
-    title: "My Project Title 7",
+    imgALT: "A Temperature Converter",
+    title: "Temperature Converter",
     techStack: "Python",
-    description: "Pokedex",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "converter-modal",
+    description:
+      "Defining functions in python to create a program converting temperatures fahrenheit to celcius and vice versa.",
+    githubURL:
+      "https://github.com/abdul-malik360/temperature_converter/blob/main/temp_converter-task.py",
+    liveProjectURL:
+      "https://replit.com/@abdulmalik360/Temperature-Converter#main.py",
+    id: "temp-converter",
   },
   {
     imgURL: "../images/projects/patientapp.PNG",
-    imgALT: "My Project 7",
-    title: "My Project Title 7",
+    imgALT: "A patient app",
+    title: "Sick Patient App",
     techStack: "Python",
-    description: "Pokedex",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "patient-modal",
+    description: "A patient app",
+    githubURL:
+      "https://github.com/abdul-malik360/Creating_a_class/blob/main/oop_task.py",
+    liveProjectURL:
+      "https://replit.com/@abdulmalik360/Creatingaclass#oop_task.py",
+    id: "patient-app",
   },
   {
     imgURL: "../images/projects/pokedex.png",
-    imgALT: "My Project 7",
-    title: "My Project Title 7",
+    imgALT: "A Pokedex",
+    title: "Pokedex",
     techStack: "JavaScript",
-    description: "Pokedex",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "pokedex-modal",
+    description: "A Pokedex using Javascript",
+    githubURL: "https://github.com/abdul-malik360/Pokedex",
+    liveProjectURL: "https://naughty-kowalevski-9438ab.netlify.app/",
+    id: "pokedex",
   },
   {
     imgURL: "../images/projects/errorhandlingapp.PNG",
-    imgALT: "My Project 7",
-    title: "My Project Title 7",
+    imgALT: "",
+    title: "Error Handling",
     techStack: "Python",
-    description: "Pokedex",
-    githubURL: "#",
-    liveProjectURL: "#",
-    id: "error-modal",
+    description: "Savings Program",
+    githubURL:
+      "https://github.com/abdul-malik360/Error_Handling/blob/main/task.py",
+    liveProjectURL: "https://replit.com/@abdulmalik360/ErrorHandling#task.py",
+    id: "error-handling",
   },
+
+  // {
+  //   imgURL: "",
+  //   imgALT: "",
+  //   title: "",
+  //   techStack: "",
+  //   description: "",
+  //   githubURL: "",
+  //   liveProjectURL: "",
+  //   id: "",
+  // },
 ];
 
 function projectPicture(image) {
@@ -159,11 +183,20 @@ function createModal(modal) {
   <div id="${modal.id}" class="modal">
     <div class="back-drop">
 
-      <img class="modal-picture" src="${modal.imgURL}" />
-      <h4>${modal.title}</h4>
-      <h6>${modal.techStack}</h6>
-      <button onclick="toggleModal('${modal.id}')">close</button>
+      <button class="modal-btn" onclick="toggleModal('${modal.id}')">X</button>
 
+      <img class="modal-picture" src="${modal.imgURL}" />
+      <div class="modal-content"> 
+      
+        <h4 class="modal-title">${modal.title}</h4>
+        <h6 class="tech-stack">${modal.techStack}</h6>
+        <p>${modal.description}</p>
+        <div class="modal-link-buttons">
+          <a href="${modal.githubURL}" target="_blank" class="modal-link-btn">Github</a>
+          <a href="${modal.liveProjectURL}" target="_blank" class="modal-link-btn">Live</a>
+        </div>
+      
+      </div> 
     </div>
 </div>
   `;
